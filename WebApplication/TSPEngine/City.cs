fbfd448f5d41f7edb307bbb4fb01950a4a7be27f
matmil92@outlook.com <matmil92@outlook.com>
2017-04-10 11:44:12 +0200

    dodanie klasy City do TSPEngine

---
+++ WebApplication/TSPEngine/City.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TSPEngine
+{
+    public class City
+    {
+        public Double x { get; private set; }
+
+        public Double y { get; private set; }
+
+        public Int32 cityName { get; private set; }
+
+
+        public City(Int32 cityName, Double x, Double y)
+        {
+            this.x = x;
+            this.y = y;
+            this.cityName = cityName;
+        }
+
+
+
+    }
+}
