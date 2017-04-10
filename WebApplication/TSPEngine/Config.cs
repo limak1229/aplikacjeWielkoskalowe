a396491db8ef56bb55716d7a267b4b7bc20163af
matmil92@outlook.com <matmil92@outlook.com>
2017-04-10 11:58:42 +0200

    Dodanie klasy Config do TSPEngine

---
+++ WebApplication/TSPEngine/Config.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TSPEngine
+{
+    public static class Config
+    {
+        public static void Connect(this IEnumerable<Stop> stops, bool loop)
+        {
+            Stop prev = null, first = null;
+            foreach (var stop in stops)
+            {
+                if (first == null) first = stop;
+                if (prev != null) prev.Next = stop;
+                prev = stop;
+            }
+
+            if (loop)
+            {
+                prev.Next = first;
+            }
+        }
+        private static T MinBy<T, TComparable>(
+    this IEnumerable<T> xs,
+    Func<T, TComparable> func)
+    where TComparable : IComparable<TComparable>
+        {
+            return xs.DefaultIfEmpty().Aggregate(
+                (maxSoFar, elem) =>
+                func(elem).CompareTo(func(maxSoFar)) > 0 ? maxSoFar : elem);
+        }
+
+
+        public static IEnumerable<Stop> NearestNeighbors(this IEnumerable<Stop> stops)
+        {
+            var stopsLeft = stops.ToList();
+            for (var stop = stopsLeft.First();
+                 stop != null;
+                 stop = stopsLeft.MinBy(s => Stop.Distance(stop, s)))
+            {
+                stopsLeft.Remove(stop);
+                yield return stop;
+            }
+        }
+    }
+}
---
+++ WebApplication/TSPEngine/Tour.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TSPEngine
+{
+    public class Tour
+    {
+        public Tour(IEnumerable<Stop> stops)
+        {
+            Anchor = stops.First();
+        }
+
+
+        //the set of tours we can make with 2-opt out of this one
+        public IEnumerable<Tour> GenerateMutations()
+        {
+            for (Stop stop = Anchor; stop.Next != Anchor; stop = stop.Next)
+            {
+                //skip the next one, since you can't swap with that
+                Stop current = stop.Next.Next;
+                while (current != Anchor)
+                {
+                    yield return CloneWithSwap(stop.City, current.City);
+                    current = current.Next;
+                }
+            }
+        }
+
+
+        public Stop Anchor { get; set; }
+
+
+        public Tour CloneWithSwap(City firstCity, City secondCity)
+        {
+            Stop firstFrom = null, secondFrom = null;
+            var stops = UnconnectedClones();
+            stops.Connect(true);
+
+            foreach (Stop stop in stops)
+            {
+                if (stop.City == firstCity) firstFrom = stop;
+
+                if (stop.City == secondCity) secondFrom = stop;
+            }
+
+            //the swap part
+            var firstTo = firstFrom.Next;
+            var secondTo = secondFrom.Next;
+
+            //reverse all of the links between the swaps
+            firstTo.CanGetTo()
+                   .TakeWhile(stop => stop != secondTo)
+                   .Reverse()
+                   .Connect(false);
+
+            firstTo.Next = secondTo;
+            firstFrom.Next = secondFrom;
+
+            var tour = new Tour(stops);
+            return tour;
+        }
+
+
+        public IList<Stop> UnconnectedClones()
+        {
+            return Cycle().Select(stop => stop.Clone()).ToList();
+        }
+
+
+        public double Cost()
+        {
+            return Cycle().Aggregate(
+                0.0,
+                (sum, stop) =>
+                sum + Stop.Distance(stop, stop.Next));
+        }
+
+
+        private IEnumerable<Stop> Cycle()
+        {
+            return Anchor.CanGetTo();
+        }
+
+
+        public override string ToString()
+        {
+            string path = String.Join(
+                "->",
+                Cycle().Select(stop => stop.ToString()).ToArray());
+            return String.Format("Cost: {0}, Path:{1}", Cost(), path);
+        }
+
+    }
+}
