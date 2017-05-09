286f6b17343df3a1075a260b9e00a787fab56d75
kamilw1222@gmail.com <kamilw1222@gmail.com>
2017-05-09 15:08:10 +0200

    Początki fabryki obiektów

---
+++ WebApplication/src/AppCore/RouteCalculatorFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AppCore.Interfaces;
+
+namespace AppCore
+{
+    public class RouteCalculatorFactory
+    {
+        public IRouteCalculator GetRouteCalculator(string version);
+    }
+}
