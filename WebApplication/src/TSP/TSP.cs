a17db9e4480dec453cd6e0ad3ca53f042bc62119
kamilw1222@gmail.com <kamilw1222@gmail.com>
2017-05-10 15:50:38 +0200

    Poprawka algorytmu

--- WebApplication/src/TSP/TSP.cs
+++ WebApplication/src/TSP/TSP.cs
@@ -27,19 +27,25 @@ namespace TSP
             stops.Connect(true);
 
             var startingTour = new Tour(stops);
+            AddNewCalculatedRoute(token, startingTour);
 
             while (true)
             {
                 var newTour = startingTour.GenerateMutations().MinBy(tour => tour.Cost());
-
-                _calculatedRoutesRepository.AddNewCalculatedRoute("1.0", token,
-                    JsonConvert.SerializeObject(newTour.Path()));
-
                 if (newTour.Cost() < startingTour.Cost())
+                {
                     startingTour = newTour;
+                    AddNewCalculatedRoute(token, newTour);
+                }
                 else
                     return;
             }
+        }
+
+        private void AddNewCalculatedRoute(Guid token, Tour newTour)
+        {
+            _calculatedRoutesRepository.AddNewCalculatedRoute("1.0", token,
+                JsonConvert.SerializeObject(newTour.Path()));
         }
     }
 }
